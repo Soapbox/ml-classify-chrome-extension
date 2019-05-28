e3ba1419dddf1a3b07c5a614771b32f0866141c1
Aditya Chinchure <adi@soapboxhq.com>
2019-05-28 13:16:07 -0400

    Added API

--- background.js
+++ background.js
@@ -11,11 +11,11 @@ let aggregateMap = {
 };
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.api == "acceptItem") {
+  if (request.api === 'acceptItem') {
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://13.57.13.174:8081/sample", true);
+    xhr.open('POST', 'http://13.57.13.174:8081/sample', true);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
+      if (xhr.readyState === 4) {
         var resp = JSON.parse(xhr.responseText);
         aggregateCategories(resp);
         sendResponse(resp);
@@ -24,23 +24,34 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     xhr.send(JSON.stringify({sample: request.item}));
     return true;
   }
-  if (request.api == "acceptCount") {
-
+  if (request.api === 'getBlogPosts') {
+    var xhr = new XMLHttpRequest();
+    let params = getCategories();
+    xhr.open('GET', 'http://13.57.13.174:8081/get_blog'+params, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        sendResponse(xhr.responseText);
+      }
+    };
+    xhr.send(null);
+    return true;
   }
 });
 
-let aggregateCategories = (resp) => {
+function getCategories() {
+  let props = Object.keys(aggregateMap).map(function(key) {
+    return { key: key, value: this[key] };
+  }, aggregateMap);
 
+  props.sort(function(p1, p2) { return p1.value - p2.value; });
+
+  return "?first_category="+props[0].key+"&second_category="+props[1].key;
+}
+
+function aggregateCategories(resp) {
   let response = JSON.parse(resp);
   for (let key in response.categories) {
     let perc = parseFloat(response.categories[key]);
     aggregateMap[key] += perc;
   }
-
-  var props = Object.keys(aggregateMap).map(function(key) {
-    return { key: key, value: this[key] };
-  }, aggregateMap);
-  props.sort(function(p1, p2) { return p2.value - p1.value; });
-  var toptwo = props.slice(0, 2);
-  console.log(toptwo)
-};
+}
--- content.js
+++ content.js
@@ -1,5 +1,8 @@
+let counter = 0;
+let arrItems = [];
+
 function createTextNode(text, className) {
-  let node = document.createElement("span");
+  let node = document.createElement('span');
   node.classList.add(className);
   let textNode = document.createTextNode(text);
   node.appendChild(textNode);
@@ -8,21 +11,20 @@ function createTextNode(text, className) {
 
 function createBlogPost(post) {
   console.log(post);
-  let postNode = document.createElement("div");
-  postNode.classList.add("blog-post");
+  let postNode = document.createElement('div');
+  postNode.classList.add('blog-post');
   postNode.appendChild(createTextNode(post.title, 'blog-post-title'));
-  postNode.appendChild(createTextNode(post.publication_date, 'blog-post-date'));
+  postNode.appendChild(createTextNode(post.date, 'blog-post-date'));
   postNode.appendChild(createTextNode(post.category, 'blog-post-category'));
   return postNode;
 }
 
 function addBlogs(response) {
-  let parentNode = document.createElement("div");
-  parentNode.classList.add("blog-posts");
+  let parentNode = document.createElement('div');
+  parentNode.classList.add('blog-posts');
 
-  for (let i = 0, s = response.blogs.length; i < s; i++) {
-    console.log(response.blogs[i]);
-    parentNode.appendChild(createBlogPost(response.blogs[i]));
+  for (let i = 0, s = response.length; i < s; i++) {
+    parentNode.appendChild(createBlogPost(response[i]));
   }
 
   let containers = document.getElementsByClassName('page-layout__container');
@@ -53,58 +55,51 @@ function addClassification(response, i) {
     Performance: 'Performance'
   };
 
-  let parentNode = document.createElement("div");
-  parentNode.classList.add("ml-tags");
+  let parentNode = document.createElement('div');
+  parentNode.classList.add('ml-tags');
 
   for (let j = 0, s = sortResp.length; j < s; j++) {
     let perc = Math.round(parseFloat(sortResp[j][1]) * 1000) / 10;
     if (perc >= 20) {
-      let node = document.createElement("span");
+      let node = document.createElement('span');
       let textnode = document.createTextNode(mapping[sortResp[j][0]]);
       node.appendChild(textnode);
-      node.title = "Predicted: "+perc+"%";
-      node.classList.add("ml-tag");
+      node.title = 'Predicted: '+perc+'%';
+      node.classList.add('ml-tag');
       parentNode.appendChild(node);
     }
   }
 
   items[i].parentElement.appendChild(parentNode);
+  counter++;
+  if (counter === arrItems.length) {
+    getBlogPosts();
+  }
 }
+
+function getBlogPosts() {
+  chrome.runtime.sendMessage(
+      {api: 'getBlogPosts'},
+      response => addBlogs(JSON.parse(response))
+  );
+}
+
 
 setTimeout(
   function() {
-    var items = document.getElementsByClassName('link-to');
-    for (var i = 0, l = items.length; i < l; i++) {
+    let items = document.getElementsByClassName('link-to');
+    for (var i = 0; i < items.length; i++) {
       if(items[i].parentElement.className === 'list-item__body') {
-        let index = i;
-        let afterResponse = (data) => {addClassification(data, index);};
-        chrome.runtime.sendMessage(
-            {api: "acceptItem", item:items[i].innerText},
-            response => afterResponse(JSON.parse(response))
-        );
+        arrItems.push({idx: i, text: items[i].innerText});
       }
     }
 
-    let sampleResponse = {
-      blogs: [
-        {
-          title: 'This is a long post title: with no emoji',
-          publication_date: '12 May 2018',
-          category: 'Productive Meetings'
-        },
-        {
-          title: 'This is another long post title: making it more diverse',
-          publication_date: '12 May 2018',
-          category: 'Productive Meetings'
-        },
-        {
-          title: 'This is a short post title',
-          publication_date: '12 May 2018',
-          category: 'Productive Meetings'
-        }
-      ]
-    };
-
-    addBlogs(sampleResponse);
-
+    for (var j = 0; j < arrItems.length; j++) {
+      let index = arrItems[j].idx;
+      let addResponse = (response) => addClassification(response, index);
+      chrome.runtime.sendMessage(
+          {api: 'acceptItem', item: arrItems[j].text},
+          response => addResponse(JSON.parse(response))
+      );
+    }
   }, 5000);
